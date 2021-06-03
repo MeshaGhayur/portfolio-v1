4748c8daf3369154e28520df8ae8ef80366d1b71
Mesha Ghayur <meshaghayur@hotmail.com>
2021-06-03 01:45:28 -0400

    Testing - add .active based on url

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,14 @@
+(function() {
+  var nav = document.getElementById('nav-list'),
+      anchor = nav.getElementsByTagName('a'),
+      current = window.location.pathname.split('/')[1];
+      for (var i = 0; i < anchor.length; i++) {
+      if(anchor[i].href == current) {
+          anchor[i].className = "active";
+      }
+  }
+})();
+
 function toggleOpen() {
   var element = document.getElementById("nav-list");
 
