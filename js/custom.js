9bbff1e1d2b1eb100a76c304ff2db51a6fc44517
Mesha Ghayur <meshaghayur@hotmail.com>
2021-06-03 02:25:10 -0400

    Undid .active testing + moved inner page to main level

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,3 @@
-(function() {
-  var nav = document.getElementById('nav-list'),
-      anchor = nav.getElementsByTagName('a'),
-      current = window.location.pathname.split('/')[1];
-      for (var i = 0; i < anchor.length; i++) {
-      if(anchor[i].href == current) {
-          anchor[i].className = "active";
-      }
-  }
-})();
-
 function toggleOpen() {
   var element = document.getElementById("nav-list");
 
